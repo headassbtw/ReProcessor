6a5c8a3393fb17854efb0a84a5380e2267e2d932
headassbtw <cooldood909@gmail.com>
2021-05-27 12:34:22 -0700

    Add project files.

---
+++ ReProcessor/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using SiraUtil;
+using SiraUtil.Zenject;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace ReProcessor
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public Plugin(Zenjector zenjector)
+        {
+            zenjector.OnApp<MyMainInstaller>().WithParameters(10); // Use Zenject's installer parameter system!
+            zenjector.OnMenu<MyMenuUIInstaller>();
+            zenjector.OnGame<MyGameInstaller>();
+
+            // Specify the scene name or contract or installer!
+            zenject.On("Menu").Register<MyMenuEffectsInstaller>();
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+            new GameObject("ReProcessorController").AddComponent<ReProcessorController>();
+
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+    }
+}
