e51f7a2a708a86f8c4a0cd9452b02b31b91c531e
dawnvt <stablumtrym@gmail.com>
2021-05-28 16:07:14 +0200

    UI working in game, need to implement UI to config changes.

--- ReProcessor/UI/Settings/BloomSettingsView.cs
+++ ReProcessor/UI/Settings/BloomSettingsView.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.ViewControllers;
+using RuntimeUnityEditor.BSIPA4;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace ReProcessor.UI
@@ -12,6 +16,13 @@ namespace ReProcessor.UI
         void a()
         {
             
+        }
+        
+        [UIValue("Factor Value")]
+        public float Enabled
+        {
+            get => Plugin.Config.Enabled;
+            set => Plugin.Config.Enabled = value;
         }
     }
 }
