4d78b940f9e8bb98b8bc171762e24083a99b0f4a
Eris <erisapps@outlook.be>
2022-02-25 17:47:29 +0100

    Cleanup of Plugin class

--- ReProcessor/Plugin.cs
+++ ReProcessor/Plugin.cs
@@ -1,35 +1,22 @@
-﻿using System.Linq;
-using IPA;
+﻿using IPA;
 using IPA.Config;
 using IPA.Config.Stores;
+using IPA.Logging;
 using SiraUtil.Zenject;
 using ReProcessor.Configuration;
 using ReProcessor.Installers;
-using ReProcessor.Managers;
-using SiraUtil.Logging;
-using IPALogger = IPA.Logging.Logger;
 
 namespace ReProcessor
 {
-    [Plugin(RuntimeOptions.DynamicInit),
-     NoEnableDisable] // NoEnableDisable supresses the warnings of not having a OnEnable/OnStart
-    // and OnDisable/OnExit methods
+    [Plugin(RuntimeOptions.DynamicInit), NoEnableDisable] // NoEnableDisable suppresses the warnings of not having a OnEnable/OnStart and OnDisable/OnExit methods
     public class Plugin
     {
-        internal static Plugin Instance { get; private set; }
-        internal static IPALogger Log { get; private set; }
-        internal PluginConfig _config;
-
         [Init]
-        public void Init(Zenjector zenjector, IPALogger logger, Config config)
+        public void Init(Zenjector zenjector, Logger logger, Config config)
         {
-            Instance = this;
-            Log = logger;
-
             zenjector.UseLogger(logger);
             zenjector.UseMetadataBinder<Plugin>();
 
-            
             zenjector.Install<AppInstaller>(Location.App, config.Generated<PluginConfig>());
             zenjector.Install<MenuInstaller>(Location.Menu);
         }
