74fec60e5b955405f82d53734ca1fc7715554d20
Eris <erisapps@outlook.be>
2022-02-25 18:41:13 +0100

    Minor changes in ButtonManager

--- ReProcessor/UI/ButtonManager.cs
+++ ReProcessor/UI/ButtonManager.cs
@@ -3,10 +3,8 @@ using IPA.Loader;
 using SiraUtil.Zenject;
 using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using ReProcessor.Managers;
 using SiraUtil.Logging;
 using Tweening;
 using UnityEngine;
@@ -17,16 +15,16 @@ using Zenject;
 
 namespace ReProcessor.UI
 {
-    public class ButtonManager : IInitializable, IDisposable
+    internal class ButtonManager : IInitializable, IDisposable
     {
-#pragma warning disable 8632 //shut the fuck up rider
         private ClickableImage? _image;
         public event Action? WasClicked;
-#pragma warning restore 8632 //shut the fuck up rider
+
         private readonly Assembly _assembly;
-        private SiraLog _log;
+        private readonly SiraLog _log;
         private readonly DiContainer _container;
         private readonly TimeTweeningManager _tweeningManager;
+
         private readonly LevelSelectionNavigationController _levelSelectionNavigationController;
         //private static readonly Color _emptyColor = new Color(0.15f, 0f, 0f, 1f);
 
@@ -38,6 +36,7 @@ namespace ReProcessor.UI
                 {
                     return _image.DefaultColor;
                 }
+
                 return null;
             }
             set
@@ -48,7 +47,7 @@ namespace ReProcessor.UI
                     _tweeningManager.KillAllTweens(_image);
                     var tween = new FloatTween(0f, 1f, val => _image!.color = Color.Lerp(oldColor, value.Value, val), 1f, EaseType.InOutSine);
                     _tweeningManager.AddTween(tween, _image);
-                    tween.onCompleted = delegate () { _image!.DefaultColor = value.Value; };
+                    tween.onCompleted = delegate { _image!.DefaultColor = value.Value; };
                 }
             }
         }
@@ -76,9 +75,11 @@ namespace ReProcessor.UI
             if ((rng.Equals(3)) && (DateTime.UtcNow.Month.Equals(6)))
                 variant = "3";
             _image = CreateImage();
-            using Stream mrs = _assembly.GetManifestResourceStream($"ReProcessor.Resources.MenuButtonImages.untitled{variant}.png");
-            using MemoryStream ms = new MemoryStream();
-            await mrs.CopyToAsync(ms);
+            using var ms = new MemoryStream();
+            using (var mrs = _assembly.GetManifestResourceStream($"ReProcessor.Resources.MenuButtonImages.untitled{variant}.png")!)
+            {
+                await mrs.CopyToAsync(ms);
+            }
 
             _image.OnClickEvent += Clicked;
             _image.sprite = BeatSaberMarkupLanguage.Utilities.LoadSpriteRaw(ms.ToArray());
@@ -112,10 +113,10 @@ namespace ReProcessor.UI
             gameObject.AddComponent<LayoutElement>();
 
             var canvas = gameObject.AddComponent<Canvas>();
-            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord1;
-            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord2;
-            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.Tangent;
-            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.Normal;
+            canvas.additionalShaderChannels |= AdditionalCanvasShaderChannels.TexCoord1 
+                                               | AdditionalCanvasShaderChannels.TexCoord2
+                                               | AdditionalCanvasShaderChannels.Tangent
+                                               | AdditionalCanvasShaderChannels.Normal;
             _container.InstantiateComponent<VRGraphicRaycaster>(gameObject);
 
             return image;
